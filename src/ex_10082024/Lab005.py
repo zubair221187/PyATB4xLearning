368beca08180df25c7f804bc0a390b1614948e4d
zubair221187 <zubair221187@gmail.com>
2024-08-12 17:37:57 +0530

    Lab005 added

---
+++ src/ex_10082024/Lab005.py
@@ -0,0 +1,6 @@
+#1+1=2 - Syntax error
+#Source Code -  Human readable code, code written by humans - HLL
+#Source Code - We have to follow some rules -  Python rules
+
+print("Good","Abdullah",sep='?',end='_')
+print("Zubair")
