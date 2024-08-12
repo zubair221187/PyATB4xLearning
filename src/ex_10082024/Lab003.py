2eea247acfbf1c9dc59e209b5813b478dbd04762
zubair221187 <zubair221187@gmail.com>
2024-08-12 17:29:25 +0530

    Lab003 added

---
+++ src/ex_10082024/Lab003.py
@@ -0,0 +1,17 @@
+# Python Program
+print("Hello World")
+
+# Builtsin.py all important already built by python
+# these are like utilities
+
+# print() -
+# self - concept in OOPs which points itself
+# *args - unlimited number of arguments * - string , int ,float , boolean.
+# file=None - File IO
+
+
+# sep=' ' - how you want to separate the arguments
+# end='\n' - in the end what you want to do
+
+print("Hello", "World", "Zubair", "Abdullah", 324, 3.14, True, sep='||', end='    ')
+print("Zubair")
