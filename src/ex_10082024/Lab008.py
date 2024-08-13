6ff65c4008e968401b0df3ee5beec11dbc705b60
zubair221187 <zubair221187@gmail.com>
2024-08-13 10:27:34 +0530

    Lab008 added

---
+++ src/ex_10082024/Lab008.py
@@ -0,0 +1,17 @@
+import keyword
+print(keyword.kwlist)
+
+#variable name ?
+
+#['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class',
+# 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global',
+# 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise',
+# 'return', 'try', 'while', 'with', 'yield']
+
+
+#variable - It is used to store the value - containers
+
+age=65
+print(age)
+age=90
+print(age)
