8270efe3a1f5e70c7dd7362b0c3bdecac6a7803b
zubair221187 <zubair221187@gmail.com>
2024-08-13 10:41:52 +0530

    Lab009 added

---
+++ src/ex_10082024/Lab009.py
@@ -0,0 +1,34 @@
+age=65
+
+#age - variable - identifier
+#123 = 34 --> Not possible - A-Z , a-z
+
+# They start with a letter (A-Z or a-z)
+# an underscore (_) followed by zero or more letters,
+# underscores, and digits (0-9)
+# Python is case-sensitive,
+# so myVariable and myvariable are two different identifiers
+
+a = 10
+_ = 5
+_ = _+1
+print(_)
+
+abc123 = 78
+#123abc = 90
+_zubair = 'Abdullah'
+_123 = 23
+#$12=56
+#&23=75
+
+pi = 3.14
+name = "Zubair"
+isMale = True
+
+complex_number = 2 + 3j
+
+print(type(pi))
+print(type(name))
+print(type(isMale))
+print(complex_number.real)
+print(complex_number.imag)
