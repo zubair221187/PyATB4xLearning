89c01608986cbf184c01b555fbc396bf759e4d93
zubair221187 <zubair221187@gmail.com>
2024-08-13 10:45:56 +0530

    Lab010 added

---
+++ src/ex_10082024/Lab010.py
@@ -0,0 +1,10 @@
+# Dynamically typed
+# At the run time, data of the variable can changed, and you don't need to
+# mention the data type - Python - Int is very smart
+
+age = 45
+print(type(age))
+age = 'Zubair'
+print(age)
+print(type(age))
+
