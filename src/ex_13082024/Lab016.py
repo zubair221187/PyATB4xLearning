f21adfb3d561e8a27531c79e1e9463074fd1e774
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-07 18:06:17 +0530

    Lab016 added

---
+++ src/ex_13082024/Lab016.py
@@ -0,0 +1,26 @@
+#int - whole numbers --> -1 , 2, 100 , 1999999 , 999999
+# float --> decimal numbers --> 3.14 , 41.233 , 18.45
+# boolean --> True or False --> are You married ?
+# String -- > str - bunch of chars -->  zubair , abdullah, @#$%, ""
+"""
+We are discussing Data Types
+int , float , str , bool
+
+Advance --> list , set , tuple , dict ( will learn in future)
+"""
+
+age=36
+print(type(age)) # <class 'int'>
+
+name = 'Zubair'
+name2 = "Zubair"
+print(type(name)) # <class 'str'>
+print(type(name2)) # <class 'str'>
+
+"""
+This is Multiple line comment
+Here the code you write will not be executed
+"""
+
+
+
