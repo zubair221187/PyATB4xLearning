e9f8b5c98e44613e1aa07b1e21efe57303495973
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-07 18:22:08 +0530

    Lab017 added

---
+++ src/ex_13082024/Lab017.py
@@ -0,0 +1,24 @@
+age = 65 # int
+print (age)
+# age --> variable name --> identifier
+# = --> assignment operator ( right to left )
+# 65 --> variable value --> literal
+
+age = 95
+print (age)
+
+#what is the range of the int
+
+a= 99999999999999 # range for the int, float is very large - Python Int - automatically adjust until it has memories
+print(type(a))
+print(a)
+
+gst = 18.45
+print(type(gst))
+print(gst)
+
+result = max (45,65)
+print(result)
+
+
+
