66365bee6fe5eb554b5d9dcc9d7c883b7d76f0d2
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:41:27 +0530

    Lab030 added

---
+++ src/ex_17082024/Lab030.py
@@ -0,0 +1,17 @@
+# Operators
+
+age = +65
+print(age)
+# Assignment --> =  which is used to store the righ value to the left value ref.
+
+# - - unary operator -
+how_many_lambo_zubair = -1
+print(how_many_lambo_zubair)
+
+# Arithmetic Operator
+# +, -, *, /
+
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
