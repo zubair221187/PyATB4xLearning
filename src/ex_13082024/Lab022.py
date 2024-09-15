dae80dd58df20ed70dc9e35a91bab3eaafb0e632
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:27:31 +0530

    Lab022 added

---
+++ src/ex_13082024/Lab022.py
@@ -0,0 +1,13 @@
+# Take a user inputs a,b and calculate the sum, mul, div & sub
+# Calculator Program
+
+num1= int(input("Enter the num 1 : "))
+num2= int(input("Enter the num 2 : "))
+
+print(type(num1))
+print(type(num2))
+
+print("Sum is :", num1+num2)
+print("Sub is :", num1-num2)
+print("Mul is :", num1*num2)
+print("Div is :", num1/num2) # Division result is always in decimal
