6f523aa8edc8f6cfe76d635b4ef60eb4570c4a8f
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:56:02 +0530

    Task2 added

---
+++ src/Tasks/Task2.py
@@ -0,0 +1,11 @@
+num1=float(input("Enter Num1 :"))
+num2=float(input("Enter Num2 :"))
+
+print(f"The max. of two number is :",max(num1,num2))
+print(f"The power of {num1} to the {num2} is",pow(num1,num2))
+print("\n")
+
+print(f"SUM : {num1} + {num2} = ",num1+num2)
+print(f"SUBTRACTION : {num1} - {num2} = ",num1-num2)
+print(f"MULTIPLICATION : {num1} * {num2} = ",num1*num2)
+print(f"DIVISION : {num1} / {num2} =",num1/num2)
