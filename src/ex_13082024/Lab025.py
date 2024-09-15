7d88a4cb58d58bcd07ab8ea07ff87d371e1dd8dc
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:32:08 +0530

    Lab025 added

---
+++ src/ex_13082024/Lab025.py
@@ -0,0 +1,51 @@
+# Strings
+
+name = 'Zubair'
+#str
+print(type(name))
+print(name.upper())
+print(name.lower())
+print(len(name))
+
+
+a = '90'
+age = 90
+print(type(a))
+print(type(age))
+
+
+name = "This is a Big line"
+print(type(name))
+#name = name + 1  -- not possible to add integer to a string
+name = name + str(1)
+print(name)
+
+#Concatenation
+
+first_name = "Zubair"
+last_name = "Hangargekar"
+fullname = first_name + last_name
+print(fullname)
+
+#Type - None
+how_many_planes_i_have= None
+print(type(how_many_planes_i_have))
+#null is not present in Python
+
+age = 10
+age2 = 10
+print(id(age)) #140719754365656
+print(id(age2)) #140719754365656
+
+#Task 2
+"""
+1. Create a program, take 2 inputs from the user num1, num2 and give them
+2. max
+3. pow num1 to num2
+4. sub , mul , sum , div.
+5. Format your output with f{""}
+
+You need to share this via the GITHUB repo.
+
+
+"""
