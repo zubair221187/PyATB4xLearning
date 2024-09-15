ea27e602d2b6070e065b878351f083dac358f320
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:45:26 +0530

    Lab032 added

---
+++ src/ex_17082024/Lab032.py
@@ -0,0 +1,29 @@
+# Operators
+
+# Not Operator
+
+is_zubair_married = True
+print(not is_zubair_married) # ! - Java
+
+# is - This operator doesn't exist in Java
+
+# Logical Operator
+#> , < , >= , <=
+
+x=10
+y =10
+print( x > y)
+print ( x < y)
+
+
+a=10
+b=10
+print(a>=b) # 10 > 10 or 10 = 10
+print(a==b) # 10 = 10
+
+# OR or And GATE
+f = False
+t = True
+
+print(f or t)
+print( f and t)
