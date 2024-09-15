83b32407d3ede5796aa4caddb61953d61bc160e0
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:51:58 +0530

    Lab039 added

---
+++ src/ex_20082024/Lab039.py
@@ -0,0 +1,15 @@
+# Problem to find the max between two
+
+# Logic Building Formula
+
+# 1. user inputs -> two integers
+# 2. o/p -> int , which is max will be returned
+# 3. - input methode
+
+num1 = float(input("Enter the num1\n"))
+num2 = float(input("Enter the num2\n"))
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
