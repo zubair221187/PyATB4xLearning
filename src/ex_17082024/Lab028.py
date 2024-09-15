fed8797979cc7b2595f9d6063f5c8cbd91c09979
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:39:34 +0530

    Lab028 added

---
+++ src/ex_17082024/Lab028.py
@@ -0,0 +1,19 @@
+#Escape Sequence
+
+print("Hello World")
+print("Hello\nWorld") # \n --> new line
+print("Hello\tWorld") # \t --> tabline
+print("Hello\bWorld") # \b --> backspace
+
+# Concept of "r" -- raw string
+# Automation API, Web Automation , file_path = r concept
+dir = r'C:\zubair\n.txt'
+dir2 = r"C:\zubair\n.txt"
+print(dir)
+print(dir2)
+
+name = 'zubair\'hangargekar'
+name2= "zubair'hangargekar"
+
+print(name)
+print(name2)
