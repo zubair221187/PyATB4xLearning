8e34c8f47e0c568324e129eb5d457fc00ecf86b2
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:29:30 +0530

    Lab023 added

---
+++ src/ex_13082024/Lab023.py
@@ -0,0 +1,35 @@
+print(3/2) # 1.500
+
+number = 3.14159265359
+formated_number = f"{number:.4f}"
+print(formated_number)
+
+# Format String
+
+num=90
+print("This is the number you are working with : "f"{num}")
+
+#Table
+#2x1 =2
+#2x2 =4
+
+table=9
+print(f"{table}*1 ={table}")
+print(f"{table}*2 ={table*2}")
+print(f"{table}*3 ={table*3}")
+print(f"{table}*4 ={table*4}")
+print(f"{table}*5 ={table*5}")
+print(f"{table}*6 ={table*6}")
+print(f"{table}*7 ={table*7}")
+print(f"{table}*8 ={table*8}")
+print(f"{table}*9 ={table*9}")
+print(f"{table}*10 ={table*10}")
+
+#Task1
+# Homework : Can you create a program I will give number
+# (user input and print table)
+#1. f"{} String format concept
+#2. User will - num int -> 10,100, -1,2,3.14
+# Out put should be like :
+# 9 x 1 = 9
+# 9 x 2 = 18 ... till 10
