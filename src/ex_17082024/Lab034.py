de22ae3427fb2d1ffea755e173e968f997bbfe43
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:47:01 +0530

    Lab034 added

---
+++ src/ex_17082024/Lab034.py
@@ -0,0 +1,14 @@
+# Ternary Operator
+
+#if condition then do this
+ #   else do that
+
+#if my_age > 18 then I can watch tiktok
+#    else I can't watch
+my_age = int(input("Enter your age"))
+print("I will go GOA" if my_age > 18 else "Can't go, Stay Home")
+
+if my_age > 18:
+    print("Go Goa")
+else:
+    print("Can't go, Stay Home")
