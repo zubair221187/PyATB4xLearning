3291c6fc101efe398fcbbe8e43d5b23bed96c580
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:35:22 +0530

    Lab026 added

---
+++ src/ex_17082024/Lab026.py
@@ -0,0 +1,19 @@
+#print("Hello World")
+#name = input("Enter your name")
+#print(name)
+
+#variable
+#It is container that stores
+#variable_name = variable_value
+
+age = 65
+print(age)
+age=age+1
+print(age)
+age+2 # not assigning value hence age value will remain 66 only
+print(age)
+
+# Data Types
+# int, float, str, bool, complex
+# Advance - list, tuple , dict , set, frozenset, noneType
+# Python OrderedDict....
