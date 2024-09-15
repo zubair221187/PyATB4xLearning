dbd80b17f890bf53e76b94c72f31e1f8e31e3fdb
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:56:35 +0530

    Task3 added

---
+++ src/Tasks/Task3.py
@@ -0,0 +1,11 @@
+a=26
+print(a)
+a=36
+print(a) # assignment operator
+
+x=7
+y=8
+print(x==y) # compare operator
+
+print(x**y) # power operator
+print(x^y) # performs XOR operation 0111 & 1000 -->   1111
