df979d78b97b9f4ad250a23a0a46196e419b9176
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:37:48 +0530

    Lab027 added

---
+++ src/ex_17082024/Lab027.py
@@ -0,0 +1,34 @@
+# Literals
+
+# variable_name = variable_value
+# variable_name --> identifier
+# = --> operator --> assignment operator
+# variable_value --> literal
+# Python is dynamic type of language - literals value are determine the data type
+age = 65  # int
+is_zubair_married = True # bool
+
+# decimal system 10 --> base --> 10
+
+#binary --> base system --> 2
+
+binary_number = 0b1010 # 10
+
+# Octal --> base 8
+
+o = 0o130 # - 88
+
+# Hex --> base 16
+d = 0x12c
+
+pi = 3.14 # float
+
+name = 'Zubair' # str
+
+complex_number = 1 +7j
+
+#list
+my_list = ["zubair","abdullah","bareerah"]
+
+#tuple
+my_tuple = ("bread","apple","milk")
