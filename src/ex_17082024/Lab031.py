f889b0444cdb4ce66ac43daa8faa99ada8db8c94
Zubair Hangargekar <zubair221187@gmail.com>
2024-09-15 22:44:22 +0530

    Lab031 added

---
+++ src/ex_17082024/Lab031.py
@@ -0,0 +1,16 @@
+# x//y
+# x%y
+# x**y
+# ==
+
+print(5//2) # Quotient
+print(5%2) # Remainder
+
+# // --> Quotient
+# / --> Division
+
+print(2**4) # Power
+
+# == Compare Operator
+print(2==2)
+print(2==3)
