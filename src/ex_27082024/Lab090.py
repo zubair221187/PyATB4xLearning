5866295872339e001e165fe1c0f7f6694b6098bc
Zubair Hangargekar <zubair221187@gmail.com>
2024-10-26 12:01:08 +0530

    File 90 added

---
+++ src/ex_27082024/Lab090.py
@@ -0,0 +1,5 @@
+# __init__.py
+
+# create dir - normal folder - python doesn't search the files here or code here.
+# python is very , folder with file __int__.py - python will search the code here
+# __init__.py --> dir -> module ( where python will search better )
