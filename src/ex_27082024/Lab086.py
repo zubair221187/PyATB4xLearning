b70b2ac6147e2a2cb2f9c30c0a84290c7aa175d3
Zubair Hangargekar <zubair221187@gmail.com>
2024-10-26 12:00:24 +0530

    File 86 added

---
+++ src/ex_27082024/Lab086.py
@@ -0,0 +1,12 @@
+# lambda expression
+# A lambda is an anonymous function that returns some form of data
+
+# def triple_me(num):
+#    return num ** 3
+# output = triple_me(10)
+# print(output)
+
+output = lambda num: num ** 3
+print(output(10))
+
+# lambda is used when one liner code is present in function
